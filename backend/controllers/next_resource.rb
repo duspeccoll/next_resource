3b10d7a226d586c32d4175e34825d14fb067881e
duspeccoll <kevin.clair@du.edu>
2015-12-03 11:23:53 -0700

    cleanup, label changes

--- backend/controllers/next_resource.rb
+++ backend/controllers/next_resource.rb
@@ -7,13 +7,11 @@ class ArchivesSpaceService < Sinatra::Base
     .permissions(["view_repository"])
     .returns([200, "OK"]) \
   do
-		dataset = CrudHelpers.scoped_dataset(Resource, {}).map { |rec| rec[:id_0] }.delete_if { |x| !x.start_with?(params[:range]) }
-		dataset.delete_if { |x| x == "U901" } # we use U901 as a placeholder; here we ignore it
-		id = dataset.max.sub("#{params[:range]}", "").to_i
-		id = id + 1
-		next_id = params[:range].concat(id.to_s.rjust(3, '0'))
-
-
+    dataset = CrudHelpers.scoped_dataset(Resource, {}).map { |rec| rec[:id_0] }.delete_if { |x| !x.start_with?(params[:range]) }
+    dataset.delete_if { |x| x == "U901" } # we use U901 as a placeholder; here we ignore it
+    id = dataset.max.sub("#{params[:range]}", "").to_i
+    id = id + 1
+    next_id = params[:range].concat(id.to_s.rjust(3, '0'))
     json_response({:next => next_id})
   end
 
